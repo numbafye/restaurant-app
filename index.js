9f4c9933154f5905f368887ad0bbbea683c9dc4f
numbafye <saadiqgp@gmail.com>
2023-04-13 18:38:40 -0400

    finished javascript

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const addBtn = document.querySelectorAll('.add');
-addBtn.forEach(e => e.addEventListener('click', Counter));
-// addBtn.onclick = Counter;
+
+addBtn.forEach(e => e.addEventListener('click', () => {
+    Counter();
+    Appear();
+}));
 
 const clicks = document.querySelector('.clicks');
 
 let items = 0;
+
+function Appear() {
+    clicks.style.visibility = 'visible';
+}
 
 function Counter() {
     items += 1;
